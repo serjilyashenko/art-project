1928883bfbf5f1178e3dfa592bed648bc758eafa
Alexandra Potseluiko <a.potseluiko@gmail.com>
2017-04-30 12:46:06 +0300

    Add section in article

--- main/article/models.py
+++ main/article/models.py
@@ -1,9 +1,11 @@
 # ~*~ coding: utf-8 ~*~
 from django.db import models
 from general.models import AppModel
+from main.section.models import Section
 
 
 class Article(AppModel):
+    section = models.ForeignKey(Section, null=True, verbose_name="Раздел")
     title = models.CharField("Заголовок", max_length=255, blank=True)
     text = models.TextField("Содержание", blank=True)
     created_at = models.DateTimeField("Дата создания", auto_now_add=True)
