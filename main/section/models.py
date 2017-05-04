0b22bf2d462fd206ff381f9afe4c0a1070427cc7
Alexandra Potseluiko <a.potseluiko@gmail.com>
2017-05-04 23:02:00 +0300

    main section images :)

--- main/section/models.py
+++ main/section/models.py
@@ -7,7 +7,6 @@ class Section(AppModel):
     parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='children',
                                verbose_name='Родительский раздел')
     title = models.CharField('Заголовок', max_length=255, blank=True)
-    photo = models.TextField('Картинка', max_length=500, blank=True)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
 
     class Meta:
