305051e4aca9e1807580dba48e12b0470c4768dd
Alexandra Potseluiko <a.potseluiko@gmail.com>
2017-05-20 22:33:24 +0300

    Add robots.txt

--- main/index/urls.py
+++ main/index/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.home, name='main_index_home'),
     url(r'^sitemap$', views.sitemap, name='main_index_sitemap'),
     url(r'^contacts$', views.contacts, name='main_index_contacts'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
